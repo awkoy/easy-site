9d26cd5f8a5919ed6212036519d6e607a4a18a6b
Boiko Yaroslav <y.boiko@easyget.com.ua>
2020-09-18 14:59:57 +0300

    fix

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,4 +1,4 @@
 import firebase from "gatsby-plugin-firebase"
 
-const sendFirebase = firebase.analytics();
+const sendFirebase = firebase ? firebase.analytics() : () => {};
 export const firebaseLog = (name, params) => sendFirebase.logEvent(name, params);
